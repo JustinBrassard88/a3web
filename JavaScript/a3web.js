05f3df09ad3c3855b623c374a6da38c7c50d3cf0
JamesKwas <jam_kwas@hotmail.com>
2021-12-04 15:04:44 -0600

    buttons on plans and much more

--- JavaScript/a3web.js
+++ JavaScript/a3web.js
@@ -11,3 +11,27 @@ function imageOff(id){
   document.getElementById(id).style.display = 'none';
 
 }
+
+function priceOn(){
+  
+  document.getElementById('price1').innerHTML="$34";
+
+}
+
+function priceOn(){
+  
+  document.getElementById('price2').innerHTML="$31000000000000000000000";
+
+}
+
+function priceOn(){
+  
+  document.getElementById('price3').innerHTML="$546";
+
+}
+
+function priceOn(){
+  
+  document.getElementById('price4').innerHTML="$1243123123";
+
+}
