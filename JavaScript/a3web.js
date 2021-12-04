6585b443f6815278b67074a925a1054d438f7da5
JamesKwas <jam_kwas@hotmail.com>
2021-12-04 11:13:10 -0600

    Images on off

--- JavaScript/a3web.js
+++ JavaScript/a3web.js
@@ -1,9 +1,13 @@
-function picture(){ 
-  var pic = "Images/Trent.png"
-  document.getElementById('trenton').src = pic;
 
-  }
-     //Function that is called when the user clicks on the image
-     function hide_image(id){
-      document.getElementById(id).style.display = 'none';
-  }
+function imageOn(id){
+  
+  document.getElementById(id).src='Images/Trent.png'
+  document.getElementById(id).style.display = 'inline';
+
+}
+
+function imageOff(id){
+  
+  document.getElementById(id).style.display = 'none';
+
+}
