be79d3a46375e3ffa885c99daa39ff4a8311f9a5
JamesKwas <jam_kwas@hotmail.com>
2021-12-04 10:28:20 -0600

    hide image java

---
+++ JavaScript/a3web.js
@@ -0,0 +1,9 @@
+function picture(){ 
+  var pic = "Images/Trent.png"
+  document.getElementById('trenton').src = pic;
+
+  }
+     //Function that is called when the user clicks on the image
+     function hide_image(id){
+      document.getElementById(id).style.display = 'none';
+  }
