e7cbba61af2cf34e8883de5da502578b03f31300
JustinBrassard <justinbrassard3@gmail.com>
2021-12-04 18:38:36 -0500

    Javascript

--- JavaScript/a3web.js
+++ JavaScript/a3web.js
@@ -1,37 +1,25 @@
 
 function imageOn(id,img){
-  
   document.getElementById(id).src= img
   document.getElementById(id).style.display = 'block';
-
 }
 
 function imageOff(id){
-  
   document.getElementById(id).style.display = 'none';
-
 }
 
 function priceOn(){
-  
   document.getElementById('price1').innerHTML="$999.99";
-
 }
 
 function priceOn2(){
-  
   document.getElementById('price2').innerHTML="$499.99";
-
 }
 
 function priceOn3(){
-  
   document.getElementById('price3').innerHTML="$1,399.99";
-
 }
 
-function priceOn4(){
-  
+function priceOn4(){ 
   document.getElementById('price4').innerHTML="$699.99";
-
 }
